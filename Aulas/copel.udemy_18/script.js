70e8e194e900703c0c8849c0a26c2c34e66c6ad9
Anderson_we <andersonwecker@outlook.com>
2024-07-03 13:31:16 -0300

    Commit 03/07

--- Aulas/copel.udemy_18/script.js
+++ Aulas/copel.udemy_18/script.js
@@ -1,50 +1,83 @@
-//Método ForEach
-let nomes = ["Anderson", "Vitoria", "Rosi"];
+// //Método ForEach
+// let nomes = ["Anderson", "Vitoria", "Rosi"];
 
-nomes.forEach(nome => {
-    console.log("O nome é " + nome);
+// nomes.forEach(nome => {
+//     console.log("O nome é " + nome);
 
-});
+// });
 
-//Método includes
-let carros = ["BMW", "Fiat", "Corsa"];
+// //Método includes
+// let carros = ["BMW", "Fiat", "Corsa"];
 
-console.log(carros.includes("Fiat"));
+// console.log(carros.includes("Fiat"));
 
-if(carros.includes("Corsa") == true){
-    console.log("Carro Existente na lista.");
-}
-else
-{
-    console.log("Carro não existente na lista!!!");
-}
+// if(carros.includes("Corsa") == true){
+//     console.log("Carro Existente na lista.");
+// }
+// else
+// {
+//     console.log("Carro não existente na lista!!!");
+// }
 
-//Método Reverse 
-let arr = [1,2,3,4,5,6,7,8,9];
+// //Método Reverse 
+// let arr = [1,2,3,4,5,6,7,8,9];
 
-console.log(arr.reverse());
+// console.log(arr.reverse());
 
-//Método trim
-let text = '\n teste \n';
-console.log(text.trim());
+// //Método trim
+// let text = '\n teste \n';
+// console.log(text.trim());
 
-//Método padStart
-let milAoContrario = '1';
-console.log(milAoContrario.padStart(4, "0"));
+// //Método padStart
+// let milAoContrario = '1';
+// console.log(milAoContrario.padStart(4, "0"));
 
-//Método split
-let frase = "O meu nome é Anderson";
+// //Método split
+// let frase = "O meu nome é Anderson";
 
-let palavras = frase.split(" ");
+// let palavras = frase.split(" ");
 
-console.log(palavras);
+// console.log(palavras);
 
-//Método join
-let fraseMontada = palavras.join("<->");
+// //Método join
+// let fraseMontada = palavras.join("<->");
 
-console.log(fraseMontada);
+// console.log(fraseMontada);
 
-//Método repeat
-let repe = "Nome";
+// //Método repeat
+// let repe = "Nome";
 
-console.log(repe.repeat(5));
+// console.log(repe.repeat(5));
+
+// let listCompra = [];
+// console.log(listCompra);
+
+// listCompra.push("Arroz");
+// listCompra.push("Feijão");
+// listCompra.push("Macarrão");
+// listCompra.push("Batata");
+// listCompra.push("Sal");
+// console.log(listCompra);
+// listCompra.shift();
+// console.log(listCompra);
+
+// const numeros = [3, 7, 14, 20, 21, 29, 36, 42];
+// const num = numeros.find((element) => element % 7 === 0 && element > 10);
+// console.log(num);
+
+// const numeros1 = [5, 10, 15, 20, 25, 30, 35, 40];
+// const num1 = numeros1.filter((element1) => element1 > 20);
+// console.log(num1);
+
+// const string = " Bom dia, mundo! ";
+// const semEsp = string.trim().split(" ");
+// console.log(semEsp);
+
+// const frase = "O rato roeu a roupa do rei de roma";
+
+// const iniFrase = frase.startsWith("O");
+// console.log(iniFrase);
+
+// const endFrase = frase.endsWith("roma");
+// console.log(endFrase);
+
